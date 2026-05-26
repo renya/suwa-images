10102dcd49a73cd98ad11305231b0eadb9746d63
renya <bza11074@nifty.com>
2026-05-26 19:04:02 +0900

    add kuma-data.js suwa6.html etc

---
+++ view/kuma-data.js
@@ -0,0 +1,28 @@
+window.pointLinks = [
+
+  {
+    name: "原村5月4日",
+    type: ["bear"],
+    year: 2026,
+    lat: 35.9407174,
+    lng: 138.2260315,
+    url: "https://shimosuwa.info/pages/諏訪地方のクマ情報2026/"
+  },
+  {
+    name: "諏訪市5月17日",
+    type: ["bear"],
+    year: 2026,
+    lat: 36.01129303706496,
+    lng: 138.11578871169212,
+    url: "https://shimosuwa.info/pages/諏訪地方のクマ情報2026/"
+  },
+  {
+    name: "岡谷市5月22日",
+    type: ["bear"],
+    year: 2026,
+    lat: 36.0423717,
+    lng: 138.0541528,
+    url: "https://shimosuwa.info/pages/諏訪地方のクマ情報2026/"
+  }
+
+];
