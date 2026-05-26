9447a53598358ca16fe1fc689e4a235cda1af489
renya <bza11074@nifty.com>
2026-05-26 12:01:15 +0900

    Update map-data.js

--- view/map-data.js
+++ view/map-data.js
@@ -11,6 +11,174 @@ window.pointLinks = [
     lat: 36.067703,
     lng: 138.101420,
     url: "https://shimosuwa.info/pages/石投場/"
+  },
+  {
+    name: "ころぼっくるひゅって",
+    lat: 36.10481127580074,
+    lng: 138.18804367983242,
+    url: "https://shimosuwa.info/pages/ころぼっくるひゅって/"
+  },
+  {
+    name: "ヒュッテみさやま",
+    lat: 36.11261157740104,
+    lng: 138.17152126983885,
+    url: "https://shimosuwa.info/pages/ヒュッテみさやま/"
+  },
+  {
+    name: "下諏訪ローイングパーク AQUA未来",
+    lat: 36.06294214222031,
+    lng: 138.0801855286949,
+    url: "https://shimosuwa.info/pages/下諏訪ローイングパーク_AQUA未来/"
+  },
+  {
+    name: "下馬橋",
+    lat: 36.08027834574024,
+    lng: 138.08187598829468,
+    url: "https://shimosuwa.info/pages/下馬橋/"
+  },
+  {
+    name: "先宮神社",
+    lat: 36.054701014583564,
+    lng: 138.11722581133503,
+    url: "https://shimosuwa.info/pages/先宮神社/"
+  },
+  {
+    name: "八剱神社",
+    lat: 36.040319605718125,
+    lng: 138.12011791437484,
+    url: "https://shimosuwa.info/pages/八剱神社/"
+  },
+  {
+    name: "八島あざみ館",
+    lat: 36.119901332888595,
+    lng: 138.15954544901746,
+    url: "https://shimosuwa.info/pages/八島あざみ館/"
+  },
+  {
+    name: "八島山荘",
+    lat: 36.119859,
+    lng: 138.159379,
+    url: "https://shimosuwa.info/pages/八島山荘/"
+  },
+  {
+    name: "前宮",
+    lat: 35.993093506286,
+    lng: 138.13487541989392,
+    url: "https://shimosuwa.info/pages/前宮/"
+  },
+  {
+    name: "大灯篭",
+    lat: 36.074476291256325,
+    lng: 138.0809982087262,
+    url: "https://shimosuwa.info/pages/大灯篭/"
+  },
+  {
+    name: "御作田社",
+    lat: 36.079386633035675,
+    lng: 138.08685934660903,
+    url: "https://shimosuwa.info/pages/御作田社/"
+  },
+  {
+    name: "御湖鶴",
+    lat: 36.075530,
+    lng: 138.086504,
+    url: "https://shimosuwa.info/pages/御湖鶴/"
+  },
+  {
+    name: "慈雲寺",
+    lat: 36.082610755302994,
+    lng: 138.083821788873,
+    url: "https://shimosuwa.info/pages/慈雲寺/"
+  },
+  {
+    name: "旧御射山社",
+    lat: 36.1122684576805,
+    lng: 138.17234411289232,
+    url: "https://shimosuwa.info/pages/旧御射山社/"
+  },
+  {
+    name: "旧御射山遺跡",
+    lat: 36.113239391192934,
+    lng: 138.172485330527,
+    url: "https://shimosuwa.info/pages/旧御射山遺跡/"
+  },
+  {
+    name: "星くそ館",
+    lat: 36.15183628385667,
+    lng: 138.21089095307843,
+    url: "https://shimosuwa.info/pages/星くそ館/"
+  },
+  {
+    name: "春宮",
+    lat: 36.081362026995286,
+    lng: 138.08201063874844,
+    url: "https://shimosuwa.info/pages/春宮/"
+  },
+  {
+    name: "春宮大門",
+    lat: 36.074951,
+    lng: 138.081166,
+    url: "https://shimosuwa.info/pages/春宮大門/"
+  },
+  {
+    name: "木落し坂",
+    lat: 36.100306700042005,
+    lng: 138.09171673475774,
+    url: "https://shimosuwa.info/pages/木落し坂/"
+  },
+  {
+    name: "木落社",
+    lat: 36.10010976099958,
+    lng: 138.0916931729625,
+    url: "https://shimosuwa.info/pages/木落社/"
+  },
+  {
+    name: "来迎寺",
+    lat: 36.077251,
+    lng: 138.089459,
+    url: "https://shimosuwa.info/pages/来迎寺/"
+  },
+  {
+    name: "梶原塚",
+    lat: 36.07271473159079,
+    lng: 138.08715619147023,
+    url: "https://shimosuwa.info/pages/梶原塚/"
+  },
+  {
+    name: "橋本政屋",
+    lat: 36.06415742660286,
+    lng: 138.10850636229486,
+    url: "https://shimosuwa.info/pages/橋本政屋/"
+  },
+  {
+    name: "秋宮",
+    lat: 36.07490858768352,
+    lng: 138.09031819375076,
+    url: "https://shimosuwa.info/pages/秋宮/"
+  },
+  {
+    name: "綿の湯",
+    lat: 36.07596855875428,
+    lng: 138.09029433287844,
+    url: "https://shimosuwa.info/pages/綿の湯/"
+  },
+  {
+    name: "若宮神社",
+    lat: 36.07308774117223,
+    lng: 138.09606601217905,
+    url: "https://shimosuwa.info/pages/若宮神社/"
+  },
+  {
+    name: "魁塚",
+    lat: 36.074285,
+    lng: 138.082845,
+    url: "https://shimosuwa.info/pages/魁塚/"
+  },
+  {
+    name: "龍の口",
+    lat: 36.08086640535417,
+    lng: 138.08403119965365,
+    url: "https://shimosuwa.info/pages/龍の口/"
   }
 
 ];
